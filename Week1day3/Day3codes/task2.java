abb36a327a441b117279d88a68a719fce6003ad2
kriti-arora <92243363+kriti-arora@users.noreply.github.com>
2024-01-20 18:28:00 +0530

    Create task2.java

---
+++ Week1day3/Day3codes/task2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class task2{
+public static void main(String args[]){
+String stringnext = "K R I T I here";
+stringnext = stringnext.replace(" ","_");
+System.out.println(stringnext);
+}
+}
